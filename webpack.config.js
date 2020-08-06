6cbffef86dc2a2cc840f0fe312d32e0ae1a13226
Kevin <kevinlan321@gmail.com>
2020-08-05 22:01:42 -0700

    installed babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
 };
+
+module: {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread']
+        }
+      }
+    }
+  ]
+}
